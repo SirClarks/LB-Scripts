27df746f974d26baa0a0259a601371847b43e003
Kenny Clark <kenny.birmanov.clark@gmail.com>
2018-06-25 10:18:19 -0700

    Update runScriptMenu.py
    Added ctrl-c exit signal, and started setting up arguments

--- runScriptMenu.py
+++ runScriptMenu.py
@@ -1,3 +1,24 @@
+
+
+#package imports
+import subprocess
+import signal
+import sys
+import argparse
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--test", required=False,
+	help="testing help and argument passing")
+args = vars(ap.parse_args())
+
+if args["test"]=='plus':
+	print("yes you entered plus.")
+else:
+	print("failed")
+
+#print("Testing this {}".format(args["test"]))
+
 def print_menu():
 	print("\nRunScript Picker")
 	print("1. SublimeText")
@@ -5,39 +26,53 @@ def print_menu():
 	print("3. Gimp")
 	print("4. Substance Player")
 	print("5. Mplay")
+	print("6 ")
 	print("6. Exit")
 
-#import os
-import subprocess
+#main menu
+def menu_select():
+	try:
+		loop=True
+		while loop:
+			print_menu()
+			print("Select runscript [1-6]: ")
+			choice = raw_input()
 
-loop=True
-while loop:
-	print_menu()
-	print("Enter your choice [1-6]: ")
-	choice = raw_input()
+			if choice=='1':
+				#os.system('/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/sublime_text.sh')
+				subprocess.Popen(["bash", "/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/sublime_text.sh"])
+				sys.exit(0)
 
-	if choice=='1':
-		#os.system('/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/sublime_text.sh')
-		subprocess.Popen(["bash", "/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/sublime_text.sh"])
+			elif choice=='2':
+				#os.system('/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/spotify.sh')
+				subprocess.Popen(["bash", "/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/spotify.sh"])
+				sys.exit(0)
 
-	elif choice=='2':
-		#os.system('/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/spotify.sh')
-		subprocess.Popen(["bash", "/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/spotify.sh"])
+			elif choice=='3':
+				#os.system('/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/gimp.sh')
+				subprocess.Popen(["bash", "/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/gimp.sh"])
+				sys.exit(0)
 
-	elif choice=='3':
-		#os.system('/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/gimp.sh')
-		subprocess.Popen(["bash", "/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/gimp.sh"])
+			elif choice=='4':
+				#os.system('/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/substance_player.sh')
+				subprocess.Popen(["bash", "/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/substance_player.sh"])
+				sys.exit(0)
 
-	elif choice=='4':
-		#os.system('/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/substance_player.sh')
-		subprocess.Popen(["bash", "/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/substance_player.sh"])
+			elif choice=='5':
+				#os.system('/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/houdini_mplay.sh')
+				subprocess.Popen(["bash", "/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/houdini_mplay.sh"])
+				sys.exit(0)
 
-	elif choice=='5':
-		#os.system('/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/houdini_mplay.sh')
-		subprocess.Popen(["bash", "/LOSTBOYS/LIBRARY/TECH_CONFIG/run_scripts/houdini_mplay.sh"])
+			elif choice=='6':
+				print "\n-----exiting------\n"
+				loop=False
+				sys.exit(0)
 
-	elif choice=='6':
-		print "\n-----exiting------\n"
-		loop=False
-	else:
-		raw_input("Wrong option selection, enter any key to try again..")
+			else:
+				raw_input("Wrong option selection, enter any key to try again..")
+	except KeyboardInterrupt:
+		print("\n")
+		sys.exit(0)
+
+
+menu_select()
