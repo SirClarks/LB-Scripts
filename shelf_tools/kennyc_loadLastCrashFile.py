196e44843a9ee69da96ae06fc75055f1a7d49125
Kenny Clark <bigkenny15@yahoo.com>
2018-07-22 17:38:46 -0700

    removed unecessary comment

--- shelf_tools/kennyc_loadLastCrashFile.py
+++ shelf_tools/kennyc_loadLastCrashFile.py
@@ -4,7 +4,6 @@ import os
 
 fileList = glob.glob('/tmp/houdini_temp/crash*')
 latestFile = max(fileList, key=os.path.getctime)
-# hou.ui.displayMessage(latestFile)
 
 try:
     hou.hipFile.load(latestFile)
