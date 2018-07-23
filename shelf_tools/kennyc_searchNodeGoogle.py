175a1060020c093a7848fe72293557d32e56750b
Kenny Clark <bigkenny15@yahoo.com>
2018-07-23 10:12:21 -0400

    Add files via upload

---
+++ shelf_tools/kennyc_searchNodeGoogle.py
@@ -0,0 +1,21 @@
+import hou
+import webbrowser
+import string
+
+def getSelectedNode():
+    selected = hou.selectedNodes()
+    node = selected[-1]
+    return node
+
+# get and convert the node type to a string for search term
+n = getSelectedNode()
+foo = str(hou.Node.type(n))
+foo_list = foo.split()
+n = foo_list[-1]
+n = n[:-1]
+
+# search string combine houdini with node name
+search = 'http://www.google.com/search?q=houdini+'+n
+
+# perform websearch
+webbrowser.open(search)
