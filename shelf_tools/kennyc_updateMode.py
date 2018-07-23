a51f1f5f9b8c4c94d4e3b0ff9c0c47c13e3fb05a
Kenny Clark <bigkenny15@yahoo.com>
2018-07-22 20:35:24 -0400

    Add files via upload

---
+++ shelf_tools/kennyc_updateMode.py
@@ -0,0 +1,8 @@
+import hou
+mode = hou.updateModeSetting().name()
+
+if mode == 'AutoUpdate':
+	hou.setUpdateMode(hou.updateMode.Manual)
+if mode == 'Manual':
+	hou.setUpdateMode(hou.updateMode.AutoUpdate)
+
