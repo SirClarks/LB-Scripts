6b896d547bf43842d104b5f8b1712bc5420dcf81
Kenny Clark <bigkenny15@yahoo.com>
2018-07-22 17:32:19 -0700

    Delete kennyc_updateMode.py

--- kennyc_updateMode.py
+++
@@ -1,8 +0,0 @@
-import hou
-mode = hou.updateModeSetting().name()
-
-if mode == 'AutoUpdate':
-	hou.setUpdateMode(hou.updateMode.Manual)
-if mode == 'Manual':
-	hou.setUpdateMode(hou.updateMode.AutoUpdate)
-
