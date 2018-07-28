f31e87589f90436d3c918a1fbee38a576e335ec1
Kenny Clark <bigkenny15@yahoo.com>
2018-07-28 14:35:37 -0700

    Update kennyc_loadLastCrashFile.py
    fixed file path and added a more implicit call for .hip* file extensions, otherwise if you had a .txt in there it would try to load that and obviously fail.

--- shelf_tools/kennyc_loadLastCrashFile.py
+++ shelf_tools/kennyc_loadLastCrashFile.py
@@ -2,10 +2,11 @@ import hou
 import glob
 import os
 
-fileList = glob.glob('/tmp/houdini_temp/crash*')
+fileList = glob.glob('/home/kennyc/houdini_temp/crash*.hip*')
 latestFile = max(fileList, key=os.path.getctime)
 
 try:
     hou.hipFile.load(latestFile)
-except hou.LoadWarning, e:
-    print e
+except hou.LoadWarning, errormsg:
+    print errormsg
+
